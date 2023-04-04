d93a7c8f06d1a4f82690df04806a20631a7c2c8a
NicolasLorencini <126730205+NicolasLorencini@users.noreply.github.com>
2023-04-04 20:43:33 -0300

    Add files via upload

---
+++ Empresa/src/main/java/com/mycompany/empresa/Cliente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.empresa;
+import java.util.Date;
+/**
+ *
+ * @author aluno
+ */
+public class Cliente extends Pessoa {
+    
+    public Cliente(String nome, int idade, char cpf) {
+        super(nome, idade, cpf);
+    }
+    
+}
